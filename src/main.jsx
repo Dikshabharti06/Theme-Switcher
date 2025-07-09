eb59d329af9863eb8d2c703ca59840bdfe563c42
Dikshabharti06 <dikshabharti.db.06@gmail.com>
2025-07-09 15:40:35 +0530

    bg changes

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1 className='text-white bg-green-500 text-center'>Hey</h1>
+    <h1 className='text-white bg-green-500 text-center dark:text-green-500 dark:bg-black'>Hey</h1>
     <App/>
   </StrictMode>,
 )
