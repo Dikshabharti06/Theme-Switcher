7d0d2631102af27bdd383a64c3a4c4346c315acc
Dikshabharti06 <dikshabharti.db.06@gmail.com>
2025-07-29 02:59:18 +0530

    theme buton

--- src/components/Themebtn.jsx
+++ src/components/Themebtn.jsx
@@ -21,8 +21,8 @@ export default function ThemeBtn() {
                 onChange={onChangeBtn}
                 checked={themeMode=== "dark"}
             />
-            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-            <span className="ml-3 text-sm font-medium text-gray-900 dark:text-white">Toggle Theme</span>
+            <div className="w-12 h-8 bg-blue-100 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-blue-400 dark:peer-focus:ring-orange-700 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full after:absolute after:content-['🌤️'] after:top-[2px] after:left-[2px] after:border-gray-300 after:rounded-full after:h-5 after:w-5 after:transition-all duration-400 dark:border-gray-600 peer-checked:bg-gray-600 peer-checked:after:content-['🌙']"></div> 
+             <span className="ml-3 text-sm font-medium text-gray-900 dark:text-white">Toggle Theme</span>
         </label>
     );
 }
